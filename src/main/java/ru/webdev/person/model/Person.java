e5626f402eddba8c7357fd225e7685634a94198d
Evgeny Bayandin <5087081@gmail.com>
2024-05-29 22:51:30 +0300

    Added Annotations to Person.java in package Model

--- src/main/java/ru/webdev/person/model/Person.java
+++ src/main/java/ru/webdev/person/model/Person.java
@@ -1,7 +1,30 @@
 package ru.webdev.person.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Person {
+    @Id
+    @GeneratedValue
     private int id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String locationName;
+
+    public Person(String name, String locationName) {
+        this.name = name;
+        this.locationName = locationName;
+    }
 }
